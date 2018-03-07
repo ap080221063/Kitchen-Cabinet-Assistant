ac3f5748ff6e7f1d7a78beaae05eb13265b17b5e
ap080221063 <ap080221063@gmail.com>
2018-03-07 22:34:57 +0000

    first components

--- KCAssistant/src/app/app.module.ts
+++ KCAssistant/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchAndFilterComponentComponent } from './components/search-and-filter-component/search-and-filter-component.component';
+import { ProductListComponentComponent } from './components/product-list-component/product-list-component.component';
+import { ProductComponentComponent } from './components/product-component/product-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchAndFilterComponentComponent,
+    ProductListComponentComponent,
+    ProductComponentComponent
   ],
   imports: [
     BrowserModule
