7b27d8d6fc79f3253acc47376b6e8518fe69eaca
ap080221063 <ap080221063@gmail.com>
2018-03-09 01:01:43 +0000

    adding functionality to list of products (increase decrease quantity, remove buttons etc..)

--- KCAssistant/src/app/components/product-list/product-list.component.ts
+++ KCAssistant/src/app/components/product-list/product-list.component.ts
@@ -10,8 +10,11 @@ import { ProductService } from '../../services/product.service';
 export class ProductListComponent implements OnInit {
 
   componentProductList: Product[];
+  removeProductBtnVisible: boolean;
 
-  constructor(private prodService: ProductService) { }
+  constructor(private prodService: ProductService) {
+    this.removeProductBtnVisible = false;
+  }
 
   ngOnInit() {
     this.componentProductList = this.prodService.getAllProducts();
@@ -20,6 +23,48 @@ export class ProductListComponent implements OnInit {
   public addNewProduct() {
     let product: Product;
     product = new Product(0, '', null, 0, 0);
+
+    // make necessary function call to open the add product modal
+
+  }
+
+  public removeProductButtonVisibleToggle() {
+    let aux: boolean;
+    aux = !this.removeProductBtnVisible;
+    this.removeProductBtnVisible = aux;
+  }
+
+  public removeProduct(prodId: number) {
+    let componentProductListAux: Product[];
+    componentProductListAux = this.componentProductList;
+
+    componentProductListAux.splice(this.componentProductList.findIndex(y => y.id === prodId), 1);
+
+    this.componentProductList = componentProductListAux;
+
+  }
+
+  public productDetails(prodId: number) {
+
+    // make necessary function call to open the add product modal
+
+  }
+
+  public alterProductQty(type: string, prodId: number) {
+    for (const iterProduct of this.componentProductList) {
+      if (iterProduct.id === prodId) {
+        if (type === 'add') {
+          iterProduct.quantity = iterProduct.quantity + 1;
+        } else {
+          if (iterProduct.quantity > 0) {
+            iterProduct.quantity = iterProduct.quantity - 1;
+          }
+        }
+      }
+    }
+  }
+
+  public extractShoppingList() {
 
     // make necessary function call to open the add product modal
 
